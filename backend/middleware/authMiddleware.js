818d2102e6b1de15c67d8f839a0d5a2a1d91aa64
Rahul Saroj <115869973+Rahulsaroj02@users.noreply.github.com>
2025-09-05 21:07:11 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -10,7 +10,6 @@ export const verifyToken = async (req, res, next) => {
   }
 
   const token = authHeader.split(' ')[1];
-  console.log("this is token:",token);
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded.id).select('-password');
